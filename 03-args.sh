beb2d7f20c154af44836bbd2cf7be3bfa92e13a4
pavan <pavan@gmail.com>
2026-06-29 19:14:49 +0530

    shell

--- 03-args.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PERSON1=$1
-PERSON2=$2
-echo "$PERSON1: Hello $PERSON2....!!"
-echo "$PERSON2: Hi $PERSON1...."
-echo "$PERSON1: How are you $PERSON2 ?"
-echo "$PERSON2: I am good $PERSON1 ."
